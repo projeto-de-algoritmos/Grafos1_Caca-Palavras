c454e5f88c50ebb33643d9210be86805a4720680
Mateus de Almeida <80906504+Mateuszinnn@users.noreply.github.com>
2023-05-01 20:07:47 -0300

    Imports e first function

---
+++ jogo_caca_palavras.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import random
+from tkinter import messagebox
+from typing import List
+
+class Solution:
+
+    def __init__(self):
+        # making the possible moves in movers array
+        self.mover = [
+            [1, 0], [0, 1], [-1, 0], [0, -1],
+            [1, 1], [-1, -1], [1, -1], [-1, 1]
+        ]
